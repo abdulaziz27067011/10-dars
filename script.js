1ba8e4fea7de4b31d2d7eaeb3e4d0d7c01ef94a2
abdulaziz27067011 <abdullox30007@gmail.com>
2025-02-26 14:49:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let body = document.getElementById(`body`)
+body.style.backgroundColor = `blue`
+body.style.color = `yellow`
+body.style.display = `flex`
+
+let ism = document.getElementById(`firstname`)
+let firstname = prompt(`ism kiriting`)
+ism.innerHTML = firstname
+
+
+
+
+
+
